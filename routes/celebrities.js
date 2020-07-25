4b2d55877388835f13d1a11b174e1bc984bfd09c
Alessandro Amarri <amarri.alessandro@gmail.com>
2020-07-25 12:46:12 +0100

    Iteration 6 done

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -17,6 +17,17 @@ celebrityRouter.get('/create', (request, response, next) => {
   response.render('celebrities/create');
 });
 
+celebrityRouter.get('/:id/edit', (request, response, next) => {
+  const id = request.params.id;
+  Celebrity.findById(id)
+    .then(data => {
+      response.render('celebrities/edit', { data });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
 celebrityRouter.get('/:id', (request, response, next) => {
   const id = request.params.id;
   Celebrity.findById(id)
@@ -40,6 +51,33 @@ celebrityRouter.post('/create', (request, response, next) => {
     })
     .catch(error => {
       response.redirect('');
+    });
+});
+
+celebrityRouter.post('/:id/delete', (request, response, next) => {
+  const id = request.params.id;
+  Celebrity.findByIdAndDelete(id)
+    .then(() => {
+      response.redirect('/celebrities');
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+celebrityRouter.post('/:id/edit', (request, response, next) => {
+  const id = request.params.id;
+  const data = request.body;
+  Celebrity.findByIdAndUpdate(id, {
+    name: data.name,
+    occupation: data.occupation,
+    catchPhrase: data.catchPhrase
+  })
+    .then(() => {
+      response.redirect('/celebrities');
+    })
+    .catch(error => {
+      next(error);
     });
 });
 
